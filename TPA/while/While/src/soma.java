8c8ef63f0ae2d78f031e69abeed144ea1a2fb13b
gustav0machado <gustavomachadorocha325@gmail.com>
2023-10-05 17:31:25 -0300

    a

---
+++ TPA/while/While/src/soma.java
@@ -0,0 +1,16 @@
+
+public class soma {
+	public static void main(String[] args) {
+		int i=1;
+		int r=0;
+		int s;
+		while(i<=100){
+			r=i;
+			s=r+i;
+			
+			System.out.println(s);
+			i++;
+		}
+	}
+	
+}
