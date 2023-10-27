8da23e93d7af296503c9927ece2a4648756f0cef
gustav0machado <gustavomachadorocha325@gmail.com>
2023-10-26 21:14:46 -0300

    Create sla.java

---
+++ TPA/while/While/src/sla.java
@@ -0,0 +1,16 @@
+
+public class sla {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		int r=0, a;
+		while(i<=100) {
+			r = i+i;
+			a = i+r;
+			System.out.println(a);
+		i++;
+		}
+	}
+}
+
